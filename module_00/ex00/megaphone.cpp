796458df64ccd3b6d7591c5658d08608ca02f256
LorenFiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-08-29 15:21:27 +0200

    ex00

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,7 +6,47 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 12:41:00 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/08/29 12:41:01 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/08/29 15:20:01 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+/**
+ * @brief This program takes a string as an argument and prints it in uppercase
+ * @param argc The number of arguments passed to the program
+ * @param argv The arguments passed to the program
+*/
+
+/*
+$>./megaphone "shhhhh... I think the students are asleep..."
+SHHHHH... I THINK THE STUDENTS ARE ASLEEP...
+$>./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
+DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.
+$>./megaphone
+* LOUD AND UNBEARABLE FEEDBACK NOISE *
+$>
+*/
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			std::cout << (char)std::toupper(argv[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
