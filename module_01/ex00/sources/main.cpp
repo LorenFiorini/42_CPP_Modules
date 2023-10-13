632544f1320a1b1c53d4c67dc8d33590705381e1
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-10-13 22:30:43 +0200

    ex00 done

--- module_01/ex00/sources/main.cpp
+++ module_01/ex00/sources/main.cpp
@@ -6,39 +6,42 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 21:29:43 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/10/13 21:40:02 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/10/13 22:29:55 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-Exercise : 00
-BraiiiiiiinnnzzzZ
-Turn-in directory : ex00/
-Files to turn in : Makefile, main.cpp, Zombie.{h, hpp}, Zombie.cpp,
-newZombie.cpp, randomChump.cpp
-Forbidden functions : None
-First, implement a Zombie class. It has a string private attribute name.
-Add a member function void announce( void ); to the Zombie class. Zombies
-announce themselves as follows:
-<name>: BraiiiiiiinnnzzzZ...
-Don’t print the angle brackets (< and >). For a zombie named Foo, the message
-would be:
-Foo: BraiiiiiiinnnzzzZ...
-Then, implement the two following functions:
-• Zombie* newZombie( std::string name );
-It creates a zombie, name it, and return it so you can use it outside of the function
-scope.
-• void randomChump( std::string name );
-It creates a zombie, name it, and the zombie announces itself.
-Now, what is the actual point of the exercise? You have to determine in what case
-it’s better to allocate the zombies on the stack or heap.
-Zombies must be destroyed when you don’t need them anymore. The destructor must
-print a message with the name of the zombie for debugging purposes.
-*/
-
 #include "Zombie.hpp"
 
+static void	createZombieOnStack(void)
+{
+	std::string	name;
+
+	std::cout << "\nZombie on STACK" << std::endl;
+	std::cout << "Enter name: ";
+	std::getline(std::cin, name);
+	
+	randomChump(name);
+}
+
+static void	createZombieOnHeap(void)
+{
+	std::string	name;
+
+	std::cout << "\nZombie on HEAP" << std::endl;
+	std::cout << "Enter name: ";
+	std::getline(std::cin, name);
+	
+	Zombie	*zombie_on_heap = newZombie(name);
+	zombie_on_heap->announce();
+	delete zombie_on_heap;
+}
 int	main(void)
 {
+	std::cout << "\nWelcome to BraiiiiiiinnnzzzZ!" << std::endl;
+	std::cout << "This program creates two Zombies" << std::endl;
+	std::cout << "One on the stack and another one on the heap." << std::endl;
 	
+	createZombieOnStack();
+	createZombieOnHeap();
+	return (0);
 }
