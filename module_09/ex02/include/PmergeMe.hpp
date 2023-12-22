77d5c38f847ac94622b0b4c8ca02fc3438876b11
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-12-22 06:53:47 +0100

    Starting ex02

--- module_09/ex02/include/PmergeMe.hpp
+++ module_09/ex02/include/PmergeMe.hpp
@@ -6,13 +6,22 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 04:37:52 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/12/19 04:38:05 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/12/22 06:53:33 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
-
+class PmergeMe
+{
+    private:
+        /* data */
+    public:
+        PmergeMe(void);
+        PmergeMe(PmergeMe const &src);
+        PmergeMe &operator=(PmergeMe const &src);
+        ~PmergeMe(void);
+};
 
 #endif
--- module_09/ex02/sources/main.cpp
+++ module_09/ex02/sources/main.cpp
@@ -6,7 +6,15 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 23:56:41 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/12/19 04:33:45 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/12/22 06:52:44 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+int main(int argc, char **argv)
+{
+    // 1. Parse arguments into a vector
+    // 
+    return (0);
+}
