04ae8f13f1ee9098f3a3865814d7e159eb040d2d
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-12-22 06:34:50 +0100

    09 RPN ex01 header

--- module_09/ex01/include/RPN.hpp
+++ module_09/ex01/include/RPN.hpp
@@ -6,7 +6,40 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 04:37:00 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/12/19 04:37:00 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/12/22 06:34:32 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# include <iostream>
+# include <string>
+
+class RPN
+{
+    private:
+        std::string _expression;
+        std::string _result;
+
+        RPN(void);
+    public:
+        RPN(std::string expression);
+        RPN(RPN const &src);
+        RPN &operator=(RPN const &src);
+        ~RPN(void);
+
+
+        std::string getExpression(void) const;
+        void        setExpression(std::string expression);
+
+        std::string getResult(void) const;
+        // void        setResult(std::string result); // Maybe?
+
+        void        calculate(void);
+
+};
+
+std::ostream &operator<<(std::ostream &out, RPN const &rpn);
+
+#endif
--- module_09/ex01/sources/main.cpp
+++ module_09/ex01/sources/main.cpp
@@ -6,7 +6,24 @@
 /*   By: lfiorini <lfiorini@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 23:56:41 by lfiorini          #+#    #+#             */
-/*   Updated: 2023/12/19 04:35:12 by lfiorini         ###   ########.fr       */
+/*   Updated: 2023/12/22 06:31:41 by lfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "RPN.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        std::cout << "Usage: ./RPN \"<expression>\"" << std::endl;
+        return (1);
+    }
+
+    try {
+        RPN rpn(argv[1]);
+        std::cout << rpn << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    return (0);
+}
